8fe1fd5e62766abedcb4f1e72647c6bb8120442c
Tao Bror Bojlén <brortao@users.noreply.github.com>
2019-02-14 16:12:00 +0000

    create a welcome thread in all new groups (#11)

--- src/resolvers/Mutation/group.ts
+++ src/resolvers/Mutation/group.ts
@@ -5,14 +5,45 @@ export const group: Pick<
   MutationResolvers.Type,
   "createGroup" | "joinGroup" | "leaveGroup"
 > = {
-  createGroup: (parent, { name, description }, ctx, info) => {
+  createGroup: async (parent, { name, description }, ctx, info) => {
     // TODO: more descriptive error if name already exists
     const personId = getPersonId(ctx);
+    const dummyAuthor = await ctx.prisma.upsertPerson({
+      where: {
+        email: "wobbly@wobbly.app"
+      },
+      update: {
+        name: "Wobbly",
+        email: "wobbly@wobbly.app",
+        password: "x"
+      },
+      create: {
+        name: "Wobbly",
+        email: "wobbly@wobbly.app",
+        password: "x"
+      }
+    });
     return ctx.prisma.createGroup({
       name,
       description: description as string | undefined,
       members: {
         connect: { id: personId }
+      },
+      threads: {
+        create: {
+          title: "Welcome!",
+          posts: {
+            create: {
+              content:
+                "Welcome to your new group! Use this thread to introduce yourself.",
+              author: {
+                connect: {
+                  id: dummyAuthor.id
+                }
+              }
+            }
+          }
+        }
       }
     });
   },
